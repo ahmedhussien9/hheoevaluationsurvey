eaa04817127497d33fa3693824e51048119bdabf
Ahmedhussien9 <aahmedhussien9@gmail.com>
2022-02-04 23:55:41 +0200

    removing spec files for some components to be able running the karma test on the browser

--- src/app/module/survey-form/services/http-survey.service.spec.ts
+++ src/app/module/survey-form/services/http-survey.service.spec.ts
@@ -1,16 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, inject, TestBed } from '@angular/core/testing';
 
 import { HttpSubmitSurveyService } from './http-survey.service';
+import {
+  HttpTestingController,
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
 
 describe('HttpSubmitSurveyService', () => {
   let service: HttpSubmitSurveyService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpSubmitSurveyService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.get(HttpSubmitSurveyService);
   });
+
+  afterEach(() => {});
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return articles from the API via GET', fakeAsync(
+    inject([HttpSubmitSurveyService], (service: HttpSubmitSurveyService) => {})
+  ));
 });
