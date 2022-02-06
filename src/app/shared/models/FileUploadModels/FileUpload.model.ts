bb7ebb092bb211d4e68a0f52e535366dcead1037
Ahmedhussien9 <aahmedhussien9@gmail.com>
2022-02-06 19:24:20 +0200

    added validate for the max number of files

--- src/app/shared/models/FileUploadModels/FileUpload.model.ts
+++ src/app/shared/models/FileUploadModels/FileUpload.model.ts
@@ -7,8 +7,19 @@ export abstract class FileUploadBase implements IFileUpload {
   filesPreview: FilePreviw[];
   fileType: string;
   loading: boolean;
+  MAX_FILES_NUMBER: number = 7;
   public remove(file: FilePreviw, index: number): void {
     this.files.splice(index, 1);
+  }
+
+  public validateMaxFilesNumber(
+    upCommingFiles: NgxFileDropEntry[],
+    currentFiles: File[]
+  ) {
+    return upCommingFiles.length + currentFiles.length >
+      this.MAX_FILES_NUMBER || upCommingFiles.length > this.MAX_FILES_NUMBER
+      ? true
+      : false;
   }
 
   public add(files: NgxFileDropEntry[]): void {
